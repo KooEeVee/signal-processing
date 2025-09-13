08d3b425486593e7c0cb4bcefbc98ab06100e3ca
Kaisa <45688258+KooEeVee@users.noreply.github.com>
2025-09-13 18:39:20 +0300

    add fft functions, test signals and coverage

--- src/app.py
+++ src/app.py
@@ -1,12 +1,18 @@
-
+import numpy as np
 import fft
 
 def main():
     fs = 44100
     len = 3
-    sound = fft.record_sound(fs, len)
+    #sound = fft.record_sound(fs, len)
+    t = np.linspace(0, len, int(fs * len))
+    #sound = np.sin(2 * np.pi * 440 * t)
+    #sound = np.sin(2 * np.pi * 440 * t)+ 0.5*np.sin(2 * np.pi * 2 * 440 * t)
+    sound = np.sin(2 * np.pi * 440 * t)+ 0.5 * np.sin(2 * np.pi * 2 * 440 * t) + 0.25 * np.sin(2 * np.pi * 3 * 440 * t)
     fft.play_sound(sound, fs)
-    fft.plot_sound(sound, fs)
+    #fft.plot_time(sound, fs)
+    fft.plot_freq(sound, fs)
+    print(fft.fundamental_frequency(sound, fs))
 
 if __name__ == "__main__":
     main()
--- src/fft.py
+++ src/fft.py
@@ -14,17 +14,42 @@ def record_sound(fs, len):
 def play_sound(sound, fs):
     fs = fs
     sound = sound
+    print(sound.shape)
     sd.play(sound, fs)
     sd.wait()
 
-#Plot sound numpy array in time-domain
-def plot_sound(sound, fs):
+#Plot sound numpy array signal in time-domain
+def plot_time(sound, fs):
     sound = sound
     fs = fs
     n = sound.shape[0]
     len = n / fs
-    t = np.linspace(0., len, n)
+    t = np.linspace(0, len, n)
     plt.plot(t, sound)
     plt.xlabel("time [s]")
     plt.ylabel("amplitude")
     plt.show()
+
+#Plot sound numpy array signal magnitude spectrum in frequency-domain
+def plot_freq(sound, fs):
+    sound = sound.ravel()
+    fs = fs
+    sound_fft = np.abs(np.fft.rfft(sound))
+    n = sound.shape[0]
+    freq = np.fft.rfftfreq(n, d=1/fs)
+    plt.plot(freq, sound_fft)
+    plt.xlim(0, 5000)
+    plt.xlabel("frequency [Hz]")
+    plt.ylabel("magnitude")
+    plt.show()
+
+#Return the lowest peak (fundamental frequency) of sound numpy array signal magnitude spectrum
+def fundamental_frequency(sound, fs):
+    sound = sound.ravel()
+    fs = fs
+    sound_fft = np.abs(np.fft.rfft(sound))
+    n = sound.shape[0]
+    freq = np.fft.rfftfreq(n, d=1/fs)
+    peak = np.argmax(sound_fft)
+    f0 = freq[peak]
+    return f0
