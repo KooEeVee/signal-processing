1bd776a146e7b95070937c4f5fb13654bab6f4f0
Kaisa <45688258+KooEeVee@users.noreply.github.com>
2025-09-27 19:27:21 +0300

    add librosa and to_note function

--- src/app.py
+++ src/app.py
@@ -1,14 +1,17 @@
 import numpy as np
 import fft
+import sounds
 import math
 
 def main():
     fs = 1024 #sampling rate
-    s = 2 #length of sample sequence in seconds
+    s = 5 #length of sample sequence in seconds
+    sound = sounds.generate_sine(fs, s, 440)
+    noisy_sound = sounds.generate_noisysine(fs, s, 440, 0.5)
     #sound = fft.record_sound(fs, s)
-    t = np.linspace(0, s, int(fs * s))
-    sound = np.sin(2 * np.pi * 440 * t)
-    noisy_sound = np.sin(2 * np.pi * 440 * t) + 0.5 * np.random.uniform(-1, 1, len(t))
+    #t = np.linspace(0, s, int(fs * s))
+    #sound = np.sin(2 * np.pi * 440 * t)
+    #noisy_sound = np.sin(2 * np.pi * 440 * t) + 0.5 * np.random.uniform(-1, 1, len(t))
     fft.plot_time(noisy_sound, fs)
     fft.plot_freq(noisy_sound, fs)
     print(fft.fundamental_frequency_np(noisy_sound, fs))
@@ -21,7 +24,9 @@ def main():
     print(fft.fundamental_frequency_np(sound, fs))
     x = fft.sound_tolist(sound)
     X = fft.fft(x)
-    print(fft.fundamental_frequency_fft(X, fs))
+    f0 = fft.fundamental_frequency_fft(X, fs)
+    print(f0)
+    print(sounds.to_note(f0))
 
 
 if __name__ == "__main__":
--- src/sounds.py
+++ src/sounds.py
@@ -1,4 +1,5 @@
 import numpy as np
+import librosa
 
 #Generate test signal, fs = sampling rate, s = length of signal in seconds, f0 = fundamental frequency
 def generate_sine(fs, s, f0):
@@ -9,9 +10,14 @@ def generate_sine(fs, s, f0):
     return np.sin(2 * np.pi * f0 * t)
 
 #Generate test signal, fs = sampling rate, s = length of signal in seconds, f0 = fundamental frequency, noise_amp = noise amplitude
-def generate_sine(fs, s, f0, noise_amp):
+def generate_noisysine(fs, s, f0, noise_amp):
     fs = fs
     s = s
     f0 = f0
     t = np.linspace(0, s, int(fs * s))
-    return np.sin(2 * np.pi * f0 * t) + noise_amp * np.random.uniform(-1, 1, len(t))
+    return np.sin(2 * np.pi * f0 * t) + noise_amp * np.random.uniform(-1, 1, len(t)) # type: ignore
+
+#Convert frequencies to notes
+def to_note(freq):
+    freq = freq
+    return librosa.hz_to_note(freq)
